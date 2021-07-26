0332188f58c68623e778db0bbb3c45e2bc64c690
mac rent <macrent@macs-air-2.home>
2021-07-26 20:38:51 +1000

    motivation push

---
+++ joe-business/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+     <h1> Joe's Website coming soon </h1>
+     <p> run npm run dev to start</p>
+    </div>
+  )
+}
